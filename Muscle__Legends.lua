c16bb7b08eb4ababd97b3c2661dc02a88e1c3b90
ZyxScripts <143510821+ZyxScripts@users.noreply.github.com>
2024-11-26 16:56:48 -0600

    Update Muscle__Legends.lua

--- Muscle__Legends.lua
+++ Muscle__Legends.lua
@@ -1,8 +1,1 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local rebirthRemote = ReplicatedStorage:WaitForChild("rEvents"):WaitForChild("rebirthRemote")
-
--- Auto-fire every 0.1 seconds
-while true do
-    rebirthRemote:FireServer()  -- Send the event to the server
-    wait(0.1)  -- Wait for 0.1 seconds before firing again
-end
+game.ReplicatedStorage.rEvents.rebirthRemote:InvokeServer("rebirthRequest")
